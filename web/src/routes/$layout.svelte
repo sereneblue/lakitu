d3a67cb41a70be962d9a57e31d74bc3659a51954
sereneblue <sereneblue@users.noreply.github.com>
2021-04-06 20:58:05 -0400

    refactor: move locale handling and tailwind to base layout

--- web/src/routes/$layout.svelte
+++ web/src/routes/$layout.svelte
@@ -1,5 +1,21 @@
 <script>
-	import i18n from '../i18n';
+	import { addMessages, getLocaleFromNavigator, init } from 'svelte-i18n/dist/runtime.cjs.js';
+
+	import en from '../locales/en.json';
+
+	addMessages('en', en);
+	addMessages('en-US', en);
+
+	init({
+		fallbackLocale: 'en',
+		initialLocale: getLocaleFromNavigator()
+	});
 </script>
 
 <slot />
+
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
