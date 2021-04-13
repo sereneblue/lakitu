290bba7a3e68e4ee0f2d00b22f6c8ce9dc30efc6
sereneblue <sereneblue@users.noreply.github.com>
2021-04-12 22:32:14 -0400

    feat: add icon component

---
+++ web/src/components/base/Icon.svelte
@@ -0,0 +1,8 @@
+<script lang="typescript">
+	import feather from 'feather-icons';
+
+	export let icon: string = '';
+	export let stroke: number = 2;
+</script>
+
+{@html feather.icons[icon].toSvg({ width: '', height: '', 'stroke-width': stroke })}
