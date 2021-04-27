003b60d40c7fff695fd358aa2a507040cf30edc6
sereneblue <sereneblue@users.noreply.github.com>
2021-04-26 23:27:58 -0400

    refactor: update locale handling

--- web/src/routes/$layout.svelte
+++ web/src/routes/$layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { addMessages, getLocaleFromNavigator, init } from 'svelte-i18n/dist/runtime.cjs.js';
-
+	
 	import en from '../locales/en.json';
 
 	addMessages('en', en);
@@ -12,7 +12,11 @@
 	});
 </script>
 
-<slot />
+<div class="dark">
+	<div class="flex bg-nord6 text-nord2 dark:bg-nord0 dark:text-nord4 h-full min-h-screen px-6 py-8">
+		<slot />
+	</div>
+</div>
 
 <style global>
 	@tailwind base;
