1c364b1bd589c2170728282bddc5f42e9276fcfc
sereneblue <sereneblue@users.noreply.github.com>
2021-05-03 23:13:59 -0400

    feat(components): add modal component

---
+++ web/src/components/base/Modal.svelte
@@ -0,0 +1,152 @@
+<script lang="typescript">
+	import { createEventDispatcher } from 'svelte';
+	import { fade, fly } from 'svelte/transition';
+
+	import Icon from './Icon.svelte';
+	import Button from './Button.svelte';
+
+	type ModalType = 'warning' | 'info' | 'danger' | 'success';
+	type ModalAction = {
+		text: string;
+		hideCancel: boolean;
+		func: Function;
+	};
+
+	export let type: ModalType = 'info';
+	export let action: ModalAction;
+	export let title: string = '';
+	export let text: string = '';
+	export let show: boolean = false;
+
+	const closeModal = (): void => {
+		show = false;
+	};
+
+	const clickOutside = (node: Node, closeAction: Function): object => {
+		const handleClick = (event: Event) => {
+			let path = event.composedPath();
+
+			if (!path.includes(node)) {
+				closeAction();
+			}
+		};
+
+		setTimeout(() => {
+			document.addEventListener('click', handleClick);
+		}, 10);
+
+		return {
+			destroy() {
+				document.removeEventListener('click', handleClick);
+			}
+		};
+	};
+
+	const handleClickOutside = (node: Node): object => {
+		return clickOutside(node, closeModal);
+	};
+
+	const init = (): object => {
+		const handleClose = (e: KeyboardEvent) => {
+			if (e.key === 'Escape') {
+				closeModal();
+			}
+		};
+
+		window.addEventListener('keydown', handleClose);
+
+		return {
+			destroy() {
+				window.removeEventListener('keydown', handleClose);
+			}
+		};
+	};
+
+	const dispatch = createEventDispatcher();
+
+	$: if (!show) dispatch('close');
+</script>
+
+{#if show}
+	<div
+		class="fixed z-20 inset-0 overflow-y-auto"
+		aria-labelledby="modal-title"
+		role="dialog"
+		aria-modal="true"
+		use:init
+		transition:fade={{ duration: 250 }}
+	>
+		<div
+			class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+		>
+			<div class="fixed inset-0 bg-nord0 bg-opacity-80 transition-opacity" aria-hidden="true" />
+			<span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true"
+				>&#8203;</span
+			>
+			{#if show}
+				<div
+					use:handleClickOutside
+					in:fly={{ y: 100, duration: 250 }}
+					out:fly={{ y: 100, duration: 250 }}
+					class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+				>
+					<div class="relative bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+						<div
+							class="absolute top-0 right-0 h-6 w-6 m-4 cursor-pointer text-gray-900 opacity-25 hover:opacity-50"
+							on:click={closeModal}
+						>
+							<Icon icon="x" />
+						</div>
+						<div class="sm:flex sm:items-start">
+							<div
+								class="h-12 w-12 mx-auto flex-shrink-0 sm:mx-0 sm:h-10 sm:w-10"
+								class:text-danger-200={type === 'danger'}
+								class:text-info={type === 'info'}
+								class:text-warning={type === 'warning'}
+								class:text-success={type === 'success'}
+							>
+								{#if type === 'danger'}
+									<Icon icon="alert-octagon" />
+								{:else if type === 'info'}
+									<Icon icon="info" />
+								{:else if type === 'warning'}
+									<Icon icon="alert-triangle" />
+								{:else if type === 'success'}
+									<Icon icon="check-circle" />
+								{/if}
+							</div>
+							<div class="mt-3 w-full text-center sm:mt-0 sm:ml-4 sm:text-left">
+								<h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+									{title}
+								</h3>
+								<div class="mt-2 max-h-84">
+									{#if text}
+										<p class="text-sm text-gray-500">
+											{text}
+										</p>
+									{:else}
+										<slot />
+									{/if}
+								</div>
+							</div>
+						</div>
+					</div>
+					{#if action}
+						<div class="bg-gray-100 px-4 py-2 sm:px-6 sm:flex sm:flex-row-reverse">
+							<span class="mx-2">
+								<Button
+									text={action.text}
+									type={type === 'danger' ? 'danger' : 'primary'}
+									onClick={action.func}
+								/>
+							</span>
+							{#if !action.hideCancel}
+								<Button text="Cancel" outline flat onClick={closeModal} />
+							{/if}
+						</div>
+					{/if}
+				</div>
+			{/if}
+		</div>
+	</div>
+{/if}
