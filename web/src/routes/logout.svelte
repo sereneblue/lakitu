9b3c8f2195a7e02eb442ee5c99a505acb3582d8b
sereneblue <sereneblue@users.noreply.github.com>
2021-05-12 18:29:13 -0400

    feat(routes): add logout route

---
+++ web/src/routes/logout.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	export async function load({ page, fetch, session, context }) {
+		const res = await fetch('http://' + page.host + '/session/logout', {
+			credentials: 'include',
+		});
+
+		return {
+			status: 302,
+			redirect: '/login'
+		};
+	}
+</script>
