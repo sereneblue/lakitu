a8b47f3497f33e8e13016bc0ac8a071e5bc4fa31
sereneblue <sereneblue@users.noreply.github.com>
2021-09-14 19:42:38 -0400

    feat(service): update types

--- web/src/service/types.ts
+++ web/src/service/types.ts
@@ -3,10 +3,48 @@ export type AWSRegion = {
 	name: string;
 };
 
+export type Log = {
+	id: number;
+	event: string;
+	errorDetails: string;
+	timestamp: number;
+	status: Status;
+};
+
+export type MachineData = {
+	uuid: string;
+	instanceType: string;
+	name: string;
+	region: string;
+	size: number;
+	status: MachineStatus;
+}
+
+export type MachineEvent = {
+	event: MachineEventType;
+	machine: MachineData;
+}
+
+export type ModalAction = {
+	text: string;
+	hideCancel: boolean;
+	func: Function;
+};
+
 export type Response = {
 	success: boolean;
 	message?: string;
 	data?: object;
 };
 
+export const enum Status {
+	PENDING = 0,
+	COMPLETE = 1,
+	ERROR = 2
+};
+
+export type MachineEventType = 'start' | 'stop' | 'increaseStorage' | 'changeRegion' | 'delete';
+export type MachineStatus = 'offline' | 'online' | 'unavailable';
+export type ModalType = 'warning' | 'info' | 'danger' | 'success' | 'default';
 export type NotificationType = 'warning' | 'info' | 'danger' | 'success';
+
