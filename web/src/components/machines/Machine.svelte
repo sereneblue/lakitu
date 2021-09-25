c156fc14f973a66b140f3abce8655fad9dab9863
sereneblue <sereneblue@users.noreply.github.com>
2021-09-25 16:19:20 -0400

    feat(components): add machine components

---
+++ web/src/components/machines/Machine.svelte
@@ -0,0 +1,50 @@
+<script lang="typescript">
+	import { createEventDispatcher } from "svelte";
+	
+	import { Button, Icon } from '../base';
+	import type { MachineData, MachineEvent } from '../../service/types';
+	import MachineMenu from './MachineMenu.svelte';
+
+	export let data: MachineData;
+
+	const dispatch = createEventDispatcher();
+	
+	const emit = (event: MachineEvent): void => {
+		dispatch("emit", {
+			event,
+			machine: data
+		});
+	}
+
+	let state = {...data, menuOpen: false };
+</script>
+
+<div class="flex items-center relative rounded shadow hover:shadow-md border border-nord4 dark:border-nord2 hover:border-accent-200">
+	<div class="h-24 w-24 p-4 bg-nord4 dark:bg-nord1">
+		<Icon icon="server" stroke={1} />
+	</div>
+	<MachineMenu 
+		status={state.status} 
+		bind:open={state.menuOpen}
+		on:emit={(e) => emit(e.detail)} />
+	<div class="flex-1 space-y-1 ml-2">
+		<div class="font-semibold">{state.name}</div>
+		<div class="flex items-center text-white">
+			{#if state.status === 'offline'}
+				<div class="text-sm bg-gray-500 px-2 rounded">Offline</div>
+			{:else if state.status === 'online'}
+				<div class="text-sm bg-green-400 px-2 rounded">Online</div>
+			{:else if state.status === 'unavailable'}
+				<div class="text-sm bg-danger-100 px-2 rounded">Unavailable</div>
+			{/if}
+		</div>
+		<div class="flex items-center text-sm opacity-50">
+			{state.instanceType} / {state.size} GB / {state.region}
+		</div>
+	</div>
+	<div class="h-full">
+		<div class="mr-2 w-5 h-5">
+			<Button icon="more-vertical" transparent onClick={() => state.menuOpen = !state.menuOpen} />
+		</div>
+	</div>
+</div>
---
+++ web/src/components/machines/MachineMenu.svelte
@@ -0,0 +1,135 @@
+<script lang="typescript">
+	import { createEventDispatcher } from "svelte";
+	import { fade } from 'svelte/transition';
+
+	import { Icon } from '../base';
+	import type { MachineEvent } from '../../service/types';
+
+	export let status: MachineStatus;
+	export let open: boolean = false;
+
+	const dispatch = createEventDispatcher();
+	
+	const emit = (event: MachineEventType): void => {
+		dispatch("emit", event);
+		open = false;
+	}
+
+	const clickOutside = (node: Node, closeAction: Function): object => {
+		const handleClick = (event: Event) => {
+			let path = event.composedPath();
+
+			if (!path.includes(node)) {
+				closeAction();
+			}
+		};
+
+		setTimeout(() => {
+			document.addEventListener('click', handleClick);
+		}, 10);
+
+		return {
+			destroy() {
+				document.removeEventListener('click', handleClick);
+			}
+		};
+	};
+
+	const handleClickOutside = (node: Node): object => {
+		return clickOutside(node, () => {
+			open = false;
+		});
+	};
+</script>
+
+{#if open}
+	<div class="flex absolute top-0 right-0 mt-2 mr-2 cursor-pointer">
+		<div class="relative inline-block text-left mr-4">
+			<div
+			    transition:fade={{ duration: 150 }}
+			    use:handleClickOutside
+			    class="origin-top-right absolute right-0 w-40 z-10 rounded shadow-lg">
+			    <div class="rounded border overflow-hidden border-indigo-500 bg-white text-nord2">
+			    	{#if status === "offline" }
+						<div
+					        class="flex hover:bg-nord2/10"
+					        role="menu"
+					        aria-orientation="horizontal"
+					        aria-labelledby="options-menu">
+					        <button
+					          on:click={(e) => emit('start')}
+					          class="flex items-center w-full p-1.5 text-sm"
+					          role="menuitem">
+					          <span class="w-3 h-3 mx-1.5">
+					          	<Icon icon="play" />
+					          </span>
+					          Start
+					        </button>
+						</div>
+						<div
+					        class="flex hover:bg-nord2/10"
+					        role="menu"
+					        aria-orientation="horizontal"
+					        aria-labelledby="options-menu">
+					        <button
+					          on:click={(e) => emit('changeRegion')}
+					          class="flex items-center w-full p-1.5 text-sm"
+					          role="menuitem">
+					          <span class="w-3 h-3 mx-1.5">
+					          	<Icon icon="refresh-cw" />
+					          </span>
+					          Change Region
+					        </button>
+						</div>
+						<div
+					        class="flex hover:bg-nord2/10"
+					        role="menu"
+					        aria-orientation="horizontal"
+					        aria-labelledby="options-menu">
+					        <button
+					          on:click={(e) => emit('increaseStorage')}
+					          class="flex items-center w-full p-1.5 text-sm"
+					          role="menuitem">
+					          <span class="w-3 h-3 mx-1.5">
+					          	<Icon icon="plus" />
+					          </span>
+					          Increase Storage
+					        </button>
+						</div>
+					{:else if status === "online"}
+						<div
+					        class="flex hover:bg-nord2/10"
+					        role="menu"
+					        aria-orientation="horizontal"
+					        aria-labelledby="options-menu">
+					        <button
+					          on:click={(e) => emit('stop')}
+					          class="flex items-center w-full p-1.5 text-sm"
+					          role="menuitem">
+					          <span class="w-3 h-3 mx-1.5">
+					          	<Icon icon="square" />
+					          </span>
+					          Stop
+					        </button>
+						</div>
+					{/if}
+					<div
+				        class="flex hover:bg-nord2/10"
+				        role="menu"
+				        aria-orientation="horizontal"
+				        aria-labelledby="options-menu">
+				        <button
+				          on:click={(e) => emit('delete')}
+				          class="flex items-center w-full p-1.5 text-sm"
+				          role="menuitem">
+				          <span class="w-3 h-3 mx-1.5">
+				          	<Icon icon="trash" />
+				          </span>
+				          Delete
+				        </button>
+					</div>
+			    </div>
+			</div>
+		</div>
+	</div>
+{/if}
