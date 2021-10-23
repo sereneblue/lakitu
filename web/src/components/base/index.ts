5d55eecea8adea81e24bda0c609f52b76520d8b4
sereneblue <sereneblue@users.noreply.github.com>
2021-10-23 18:06:34 -0400

    refactor: add job progress to base components

--- web/src/components/base/index.ts
+++ web/src/components/base/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button.svelte';
 export { default as Icon } from './Icon.svelte';
+export { default as JobProgress } from './JobProgress.svelte';
 export { default as Modal } from './Modal.svelte';
 export { default as Navigation } from './Navigation.svelte';
 export { default as Notification } from './Notification.svelte';
